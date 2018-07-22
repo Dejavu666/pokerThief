1b3beec2e26d78cc2bcaa3f7419cbfbe2350dc7e
c e smith <crazyfox@cs-MacBook.local>
2018-07-22 13:48:12 -0700

    gui example, tkinter for now iguess

--- gui.py
+++ gui.py
@@ -1,5 +1,20 @@
 # GUI layer
 
+# tkinter i guess for now
+# hello mars
+# import tkinter
+# from tkinter.constants import *
+# tk = tkinter.Tk()
+# frame = tkinter.Frame(tk, relief=RIDGE,borderwidth=2)
+# frame.pack(fill=BOTH, expand=1)
+# label = tkinter.Label(frame,text='hello mars')
+# label.pack(fill=X, expand=1)
+# button = tkinter.Button(frame, text='exit',command=tk.destroy)
+# button.pack(side=BOTTOM)
+# tk.mainloop()
+
+
+
 import table
 
 table = table.Table(4,1000,20)
